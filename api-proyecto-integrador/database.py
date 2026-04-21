13896cc2cde676ce4d9e7987eafc76c35f40a726
cruiz <cruiz@gitlab.dpsit.gob.ar>
2026-04-21 14:56:26 -0300

    changed references a localDatabase in local

--- api-proyecto-integrador/database.py
+++ api-proyecto-integrador/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import settings
 import os
 
-#DATABASE_URL = f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}?client_encoding=utf8"
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
